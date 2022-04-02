178ddc3586bf9f61aa4cca17f496ea1fdd9d35de
Avinash Mamidi <avinash.mamidi548@gmail.com>
2022-04-02 05:39:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "code-sharing-bucket"
-    key    = "avinash/s3file-3.tfstate"
-    region = "us-west-2"
-    profile = "jespo"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "code-sharing-bucket"
+#     key    = "avinash/s3file-3.tfstate"
+#     region = "us-west-2"
+#     profile = "jespo"
+#   }
+# }
 
