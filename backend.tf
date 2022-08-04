84ba9578c1adfe726623051b519b0499eaa7ec1c
Avinash Mamidi <avinash.mamidi548@gmail.com>
2022-08-05 05:21:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "code-sharing-bucket"
-#     key    = "avinash/s3file-3.tfstate"
-#     region = "us-west-2"
-#     profile = "jespo"
-#   }
-# }
+ terraform {
+   backend "s3" {
+    bucket = "code-sharing-bucket"
+    key    = "avinash/s3file-3.tfstate"
+    region = "us-west-2"
+    profile = "jespo"
+dynamodb= "firstprojecttable"
+   }
+ }
 
