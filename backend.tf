6437cdc3b92bdd25f4e6c561235732f7ef68976c
Louba1 <95192017+Louba1@users.noreply.github.com>
2022-08-04 21:34:17 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 #  terraform {
 #    backend "s3" {
-#     bucket = "avinash-terraform-state"
+#     bucket = "daddy-1-terraform-state"
 #     key    = "state.tfstate"
 #     region = "us-east-1"
 #     profile = "jespo"
