8574f34561cd55b4273f7c01196a5cb092de9b18
Louba1 <95192017+Louba1@users.noreply.github.com>
2022-08-04 21:48:39 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
  terraform {
    backend "s3" {
     bucket = "code-sharing-bucket"
-    key    = "avinash/s3file-3.tfstate"
-    region = "us-west-2"
+    key    = "fred123/s3file-3.tfstate"
+    region = "us-east-1"
     profile = "jespo"
 dynamodb_table= "firstprojecttable"
    }
